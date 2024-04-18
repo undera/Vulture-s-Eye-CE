3c5584323591f9b39ffab099def0e157e17e7db3
Andrei Pohilko <andrei.pokhilko@gmail.com>
2024-04-18 17:21:19 +0100

    Improve statusbar display

--- win/vulture/winclass/statusbars.cpp
+++ win/vulture/winclass/statusbars.cpp
@@ -6,9 +6,10 @@
 #include "config.h"
 #include "global.h"
 
-extern "C" {
+extern "C"
+{
 #ifndef VULTURE_NETHACK_3_6_7
-boolean can_advance(int skill, int speedy);
+	boolean can_advance(int skill, int speedy);
 #endif
 }
 
@@ -21,7 +22,6 @@ boolean can_advance(int skill, int speedy);
 
 #include "statusbars.h"
 
-
 #define META(c) (0x80 | (c))
 
 statusbars *stbars;
@@ -29,11 +29,11 @@ int barh;
 
 statusbars::statusbars(window *p) : window(p)
 {
-	barh=vulture_get_lineheight(V_FONT_STATUS)+2;
+	barh = vulture_get_lineheight(V_FONT_STATUS) + 2;
 
 	w = 100;
-	h = barh*5;
-	
+	h = (barh + 5) * 5;
+
 	stbars = this;
 	autobg = true;
 	visible = true;
@@ -45,13 +45,21 @@ statusbars::~statusbars()
 
 bool statusbars::draw()
 {
+	float wMain = (inv_weight() + weight_cap()) / (float)weight_cap();
+	float wSec = 0;
+	if (wMain > 1)
+	{
+		wMain = 1;
+		wSec = weight_cap() / (float)(inv_weight() + weight_cap());
+	}
+
 	vulture_set_draw_region(abs_x, abs_y, abs_x + w - 1, abs_y + h - 1);
-	this->draw_bar(0, "HP", u.uhp/(float)u.uhpmax, 0, CLR32_RED);
-	this->draw_bar(1, "Magic", u.uen/(float)u.uenmax, 0, CLR32_BLESS_BLUE);
-	this->draw_bar(2, "Food", u.uhunger/(float)1000, 150/float(1000),  CLR32_ORANGE);
-	this->draw_bar(3, "Weight", (inv_weight()+weight_cap())/(float)weight_cap(), 0, CLR32_BROWN);
-	this->draw_bar(4, "XP", u.uexp/(float)newuexp(u.ulevel), 0, CLR32_LIGHTPINK);
-	vulture_set_draw_region(0, 0, vulture_screen->w-1, vulture_screen->h-1);
+	this->draw_bar(0, "HP", u.uhp / (float)u.uhpmax, 0, CLR32_RED);
+	this->draw_bar(1, "Magic", u.uen / (float)u.uenmax, 0, CLR32_BLESS_BLUE);
+	this->draw_bar(2, "Food", u.uhunger / (float)1000, 150 / float(1000), CLR32_GOLD_SHADE);
+	this->draw_bar(3, "Weight", wMain, wSec, CLR32_BROWN);
+	this->draw_bar(4, "XP", u.uexp / (float)newuexp(u.ulevel), 0, CLR32_LIGHTPINK);
+	vulture_set_draw_region(0, 0, vulture_screen->w - 1, vulture_screen->h - 1);
 
 	vulture_invalidate_region(abs_x, abs_y, w, h);
 
@@ -60,34 +68,32 @@ bool statusbars::draw()
 
 void statusbars::draw_bar(int i, std::string label, float lvlMain, float lvlSecondary, Uint32 color)
 {
-	int by=y+barh*i;
-	vulture_fill_rect(x, by, x+w*lvlMain, by+barh, color);
-	vulture_put_text(V_FONT_TOOLTIP, label, vulture_screen, x+1, by+1, CLR32_WHITE);
-	if (lvlSecondary>0) {
-		vulture_fill_rect(x+w*lvlSecondary, by, x+w*lvlSecondary, by+barh, CLR32_YELLOW);
+	int by = y + (barh + 5) * i;
+	vulture_fill_rect(x, by, x + w * lvlMain, by + barh, color);
+	vulture_put_text(V_FONT_TOOLTIP, label, vulture_screen, x + 1, by + 1, CLR32_WHITE);
+	if (lvlSecondary > 0)
+	{
+		vulture_fill_rect(x + w * lvlSecondary, by, x + w * lvlSecondary, by + barh, CLR32_YELLOW);
 	}
-	vulture_rect(x, y+barh*i, x+w, y+barh*i+barh, CLR32_GRAY70);
+	vulture_rect(x, by, x + w, by + barh, CLR32_GRAY70);
 }
 
-eventresult statusbars::handle_timer_event(window* target, void* result, int time)
+eventresult statusbars::handle_timer_event(window *target, void *result, int time)
 {
 	return V_EVENT_HANDLED_NOREDRAW;
 }
 
-
-eventresult statusbars::handle_mousemotion_event(window* target, void* result, int xrel, 
-                                             int yrel, int state)
+eventresult statusbars::handle_mousemotion_event(window *target, void *result, int xrel,
+												 int yrel, int state)
 {
 	vulture_set_mcursor(V_CURSOR_NORMAL);
 	return V_EVENT_HANDLED_NOREDRAW;
 }
 
-
-eventresult statusbars::handle_resize_event(window* target, void* result, int res_w, int res_h)
+eventresult statusbars::handle_resize_event(window *target, void *result, int res_w, int res_h)
 {
 	/* this relies on the fact that the enhance window is created
 	 * immediately after the status window */
 	y = sib_prev->y - h;
 	return V_EVENT_HANDLED_NOREDRAW;
 }
-
